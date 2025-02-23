d3495e28875ab1185965ca15845fb015d0048fd3
houstonsoper <houstonsoper@gmail.com>
2025-02-23 19:03:02 +0000

    Initialized front-end and backend

---
+++ filmspot.api/Program.cs
@@ -0,0 +1,26 @@
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.MapOpenApi();
+	app.MapScalarApiReference(options =>
+	{
+		options
+			.WithTitle("Film Spot API")
+			.WithDefaultHttpClient(ScalarTarget.CSharp, ScalarClient.HttpClient);
+	});
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+app.Run();
+
