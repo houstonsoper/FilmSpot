3cfed435e06c06d0a5071a610f68db3d743040a6
houstonsoper <houstonsoper@gmail.com>
2025-02-24 13:28:05 +0000

    Added movieService with function to fetch showings with a "Movie" interface for the response

---
+++ filmspot.frontend/services/movieService.ts
@@ -0,0 +1,16 @@
+﻿import Movie from "@/app/interfaces/movie";
+
+const BASE_URL = "https://localhost:7107/Movie";
+
+export async function fetchCurrentShowings(): Promise<Movie[]> {
+  try{
+    const response: Response = await fetch(BASE_URL);
+    if (!response.ok)
+      throw new Error("Unable to fetch movies from API");
+    
+    return await response.json();
+  } catch (e){
+    console.error(e);
+    return [];
+  }
+}
---
+++ filmspot.frontend/src/app/interfaces/movie.ts
@@ -0,0 +1,8 @@
+﻿export default interface Movie {
+  id: string,
+  title: string,
+  overview: string,
+  posterPath: string,
+  releaseDate: string,
+  isShowing: boolean,
+}
